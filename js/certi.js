404bec58f1ccae6bcb0cb48ea63698cdde4592a1
melanybaldo <melabaldovino14@gmail.com>
2025-06-04 15:27:45 -0500

    diplom

--- js/certi.js
+++ js/certi.js
@@ -1,4 +1,4 @@
-  const nombreInput = document.getElementById("nombreAlumno");
+ const nombreInput = document.getElementById("nombreAlumno");
   const nombreTexto = document.getElementById("nombreGenerado");
   const fechaTexto = document.getElementById("fechaGenerada");
 
@@ -11,24 +11,33 @@
     });
   }
 
-  nombreInput.addEventListener("input", actualizarNombre);
+  const Users = JSON.parse(localStorage.getItem("user")) || []
 
-  document.getElementById("btnDescargar").addEventListener("click", function () {
-    actualizarNombre();
+
+/*   nombreInput.addEventListener("input", actualizarNombre);  */
+
+    document.getElementById("btnDescargar").addEventListener("click", function () {
+    /* actualizarNombre(); */
     fechaTexto.textContent = formatearFechaHoy();
 
     const element = document.getElementById("certificado");
-    const nombre = nombreInput.value.trim() || "certificado";
+    /* const nombre = nombreInput.value.trim() || "certificado"; */
 
     const opt = {
       margin: 0,
-      filename: `certificado-${nombre.replace(/\s+/g, "_")}.pdf`,
-      image: { type: 'jpeg', quality: 0.98 },
+      filename: `certificado.pdf`,     /* -${nombre.replace(/\s+/g, "_")} */
+      image: { type: 'pdf', quality: 0.98 },
       html2canvas: { scale: 2 },
       jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }
     };
 
     html2pdf().set(opt).from(element).save();
   });
-  actualizarNombre();
+  /* actualizarNombre(); */
   fechaTexto.textContent = "[Fecha]";
+
+  
+
+ /*  document.getElementById ("btnDescargar").addEventListener ("click", () => {
+    const certificado = document.querySelector (".certificate")
+  } ) */
