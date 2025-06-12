5bb95a344872eeb59e2d5bb5827ee008f37e096d
melanybaldo <melabaldovino14@gmail.com>
2025-06-11 21:58:11 -0500

    modulo

--- js/modulo4.js
+++ js/modulo4.js
@@ -1,37 +1,26 @@
-document.getElementById("examen").addEventListener("submit", function(event) {
-      event.preventDefault();
+const temaOscuro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+    localStorage.setItem("tema", "dark");
+}
 
-      const respuestasCorrectas = {
-        p1: "1000",
-        p2: "madrid",
-        // Asegúrate de añadir las otras respuestas correctas aquí
-        p10: "8"
-      };
+const temaClaro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
+    localStorage.setItem("tema", "light");
+}
 
-      let puntaje = 0;
+const cambiarTema = () => {
+    const temaActual = document.querySelector("body").getAttribute("data-bs-theme");
+    temaActual === "light" ? temaOscuro() : temaClaro();
+}
 
-      for (let pregunta in respuestasCorrectas) {
-        const contenedor = document.querySelector(`input[name="${pregunta}"]`).closest(".pregunta");
-        const seleccionada = document.querySelector(`input[name="${pregunta}"]:checked`);
-        const correcta = respuestasCorrectas[pregunta];
+window.addEventListener("DOMContentLoaded", () => {
+    const temaGuardado = localStorage.getItem("tema");
 
-        // Limpia respuestas anteriores si el usuario reintenta
-        let anterior = contenedor.querySelector(".respuesta-correcta");
-        if (anterior) anterior.remove();
-
-        let mensaje = document.createElement("div");
-        mensaje.classList.add("respuesta-correcta");
-
-        if (seleccionada && seleccionada.value.toLowerCase() === correcta) {
-          puntaje++;
-          mensaje.innerHTML = `<span class="correcta">✔ Correcto</span>`;
-        } else {
-          mensaje.innerHTML = `<span class="incorrecta">✘ Incorrecto</span>. La respuesta correcta es: <strong class="correcta">${correcta}</strong>`;
-        }
-
-        contenedor.appendChild(mensaje);
-      }
-
-      const aprobado = puntaje >= 7;
-      alert(`Tu puntaje fue de ${puntaje}/10. ${aprobado ? "¡Felicidades! Aprobaste." : "Lo siento, no aprobaste."}`);
-    });
+    if (temaGuardado === "dark") {
+        temaOscuro();
+    } else {
+        temaClaro();
+    }
+});
